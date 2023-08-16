2a157b44cde557cc591edea08ef4566d8355744c
chairul <chairul.amri@ebconnect.com>
2023-08-16 11:49:28 +0700

    [1] create Blur Worker to make the image blurry [2] blur an image when the Go button is clicked

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.java
@@ -0,0 +1,53 @@
+package com.example.background.workers;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.background.R;
+
+public class BlurWorker extends Worker {
+    public BlurWorker(
+            @NonNull Context appContext,
+            @NonNull WorkerParameters workerParams) {
+        super(appContext, workerParams);
+    }
+
+    private static final String TAG = BlurWorker.class.getSimpleName();
+
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        Context applicationContext = getApplicationContext();
+
+        Bitmap picture = BitmapFactory.decodeResource(
+                applicationContext.getResources(),
+                R.drawable.test);
+
+        try {
+            // Blur the bitmap
+            Bitmap output = WorkerUtils.blurBitmap(picture, applicationContext);
+
+            // Write bitmap to a temp file
+            Uri outputUri = WorkerUtils.writeBitmapToFile(applicationContext, output);
+
+            WorkerUtils.makeStatusNotification("Output is " + outputUri.toString(), applicationContext);
+            return Result.success();
+        } catch (Throwable throwable) {
+
+            // Technically WorkManager will return Result.failure()
+            // but it's best to be explicit about it.
+            // Thus if there were errors, we're return FAILURE
+            Log.e(TAG, "Error applying blur", throwable);
+            return Result.failure();
+        }
+    }
+}
